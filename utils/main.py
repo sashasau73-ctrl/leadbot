42d7813d7b2ba2d2a85d20149df1b98affbb0991
sasha_sau <sasha.sau73@gmail.com>
2025-11-24 16:42:56 +0400

    Сделал первую домашку

--- utils/main.py
+++ utils/main.py
@@ -11,7 +11,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('').build()
+    # лох
+    application = ApplicationBuilder().token('TOKEN').build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
