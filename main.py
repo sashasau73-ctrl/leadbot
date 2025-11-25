20f9872bca853fde8aeac025ffad4b114cc6034c
sasha_sau <sasha.sau73@gmail.com>
2025-11-25 20:46:21 +0400

    новый

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import logging
+import os
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+load_dotenv()
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #update - полная иформация о том что произошло
+    #update.effective_user - иформация о человеке
+    #update.effective_chat - инормация о чате
+    #update.effective_message - информация о сообщении
+    # context - контекст, в котором мы можем использовать бота
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+        chat_id=update.effective_user.id, text=f'Привет, {update.effective_user.first_name}'
     )
+    # чтобы перезапустить бота ctrl + C
 
 
 if __name__ == "__main__":
-    application = ApplicationBuilder().token("TOKEN").build()
+    application = ApplicationBuilder().token(os.getenv('TOKEN')).build()
+    # handler - обработчик, который будет обрабатывать 
 
-    start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
+    application.add_handler(CommandHandler("start", start))
 
     application.run_polling()
